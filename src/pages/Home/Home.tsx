34eba738650caafe3d6bf81bc4f68a1b3c0310b9
João Lee <joaumlimaum@gmail.com>
2025-01-16 21:18:46 -0300

    feat: create home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import About from "../../components/About/About";
+import Intro from "../../components/Intro/Intro";
+import Main from "../../components/Main/Main";
+import Projects from "../../components/Projects/Projects";
+
+export default function Home() {
+  return (
+    <Main>
+      <Intro />
+      <About />
+      <Projects />
+    </Main>
+  )
+}
--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -1,7 +1,7 @@
-import {useHistory} from 'react-router-dom' 
+// import {useHistory} from 'react-router-dom' 
 
 export default function Project() {
- const {} = useHistory()
+//  const {} = useHistory()
   return (
     <div>Project</div>
   )
